9a4d02b85de297c0dbc2eadc5fc3f234c20e8519
Umid Davronov <umiddavronov0111@gmail.com>
2026-09-22 12:27:49 +0500

    /

--- core/settings.py
+++ core/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 # SECURITY WARNING: don't run with debug turned on in production!
+
 from decouple import Csv, config
 
 SECRET_KEY = config('SECRET_KEY')
